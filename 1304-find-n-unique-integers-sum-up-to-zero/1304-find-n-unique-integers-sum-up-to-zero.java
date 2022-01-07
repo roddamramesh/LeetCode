1e59ecc83a889900c0ca005fde094913a9cec345
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-01-06 20:40:28 -0500

    Time: 0 ms (100.00%), Space: 38.8 MB (23.88%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] A = new int[n];
+        //Find the rule A[i] = i * 2 - n + 1
+        for (int i = 0; i < n; ++i)
+            A[i] = i * 2 - n + 1;
+        return A;
+    }
+}
