48475aeac4fce7d2c5218d67bfc23886d5704a20
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-01-06 20:25:08 -0500

    Time: 50 ms (5.10%), Space: 40.4 MB (23.55%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        //Add and Remove from the same hashmap
+        HashMap<Character,Integer> hm = new HashMap<>(); 
+        
+        for(int i=0;i<s.length();i++){
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1); 
+            hm.put(t.charAt(i),hm.getOrDefault(t.charAt(i),0)-1); 
+        }
+        
+        for(Map.Entry<Character,Integer> entry :hm.entrySet()){
+               if(entry.getValue()!=0)
+                   return false;  
+        }
+        return true;
+        
+    }
+}
