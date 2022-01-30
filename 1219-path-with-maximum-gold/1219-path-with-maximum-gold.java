32a72ee5c3f41c28f87318e3ccd69cd5fe918fc5
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-01-30 01:59:24 -0500

    Time: 23 ms (52.02%), Space: 41.9 MB (10.64%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -15,30 +15,29 @@ class Solution {
         while(!queue.isEmpty()){
             int[] pair = queue.poll();
             boolean[][] visited = new boolean[grid.length][grid[0].length];
-            findMaxPath(grid,pair[0],pair[1],visited,grid[pair[0]][pair[1]],grid[pair[0]][pair[1]]+"");
+            findMaxPath(grid,pair[0],pair[1],visited,grid[pair[0]][pair[1]]);
         }
         
         return maxPathSum==Integer.MIN_VALUE?0:maxPathSum;
         
     }
     
-    public void findMaxPath(int[][] grid,int row, int col, boolean[][] visited, int pathSum, String path){
+    public void findMaxPath(int[][] grid,int row, int col, boolean[][] visited, int pathSum){
         
             if(pathSum>maxPathSum){
                 maxPathSum=Math.max(pathSum,maxPathSum);
-                System.out.println("MaxPath Sum : " + pathSum + " And  Paths is "+path);
 
             }
   
            visited[row][col]=true;
             if(row+1<grid.length && visited[row+1][col]==false && grid[row+1][col]!=0)
-                findMaxPath(grid,row+1,col,visited, pathSum + grid[row+1][col],path+"-"+grid[row+1][col]);
+                findMaxPath(grid,row+1,col,visited, pathSum + grid[row+1][col]);
              if(row-1>=0 && visited[row-1][col]==false && grid[row-1][col]!=0)
-                findMaxPath(grid,row-1,col,visited, pathSum + grid[row-1][col],path+"-"+grid[row-1][col]);
+                findMaxPath(grid,row-1,col,visited, pathSum + grid[row-1][col]);
              if(col+1<grid[0].length && visited[row][col+1]==false && grid[row][col+1]!=0)
-                findMaxPath(grid,row,col+1,visited, pathSum + grid[row][col+1],path+"-"+grid[row][col+1]);
+                findMaxPath(grid,row,col+1,visited, pathSum + grid[row][col+1]);
              if(col-1>=0 && visited[row][col-1]==false && grid[row][col-1]!=0)
-                findMaxPath(grid,row,col-1,visited, pathSum + grid[row][col-1],path+"-"+grid[row][col-1]);
+                findMaxPath(grid,row,col-1,visited, pathSum + grid[row][col-1]);
             visited[row][col]=false; 
           
     }
