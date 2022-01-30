b8803ed0df13f693d5d908f41d2961be24c216d8
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-01-29 23:35:50 -0500

    Time: 2 ms (86.97%), Space: 44.6 MB (5.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,10 +1,6 @@
 class Solution {
     int minPathSum=Integer.MAX_VALUE;
     public int minPathSum(int[][] grid) {
-        
-        int[][] dp = new int[grid.length][grid[0].length];
-        
-        
         for(int row=grid.length-1;row>=0;row--){
             for(int col= grid[0].length-1;col>=0;col--){
                 if(row<grid.length-1 &&  col<grid[0].length-1) {
