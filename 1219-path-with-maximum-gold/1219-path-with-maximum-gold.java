9c1f0dee54b0fb5298a4fbb91d888a5d93f6e098
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-01-30 02:11:12 -0500

    Time: 47 ms (15.31%), Space: 41.6 MB (11.46%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,41 +1,22 @@
 class Solution {
-    int maxPathSum=Integer.MIN_VALUE;
     public int getMaximumGold(int[][] grid) {
-        
-        Queue<int[]> queue = new LinkedList<>();
-        
-        for(int row=0;row<grid.length;row++){
-            for(int col=0;col<grid[0].length;col++){
-                if(grid[row][col]!=0){
-                    queue.offer(new int[]{row,col});
-                }
-            }
-        }
-        
-        while(!queue.isEmpty()){
-            int[] pair = queue.poll();
-            boolean[][] visited = new boolean[grid.length][grid[0].length];
-            findMaxPath(grid,pair[0],pair[1],visited,grid[pair[0]][pair[1]]);
-        }
-        
-        return maxPathSum==Integer.MIN_VALUE?0:maxPathSum;
-        
+        int m = grid.length, n = grid[0].length;
+        int maxGold = 0;
+        for (int r = 0; r < m; r++)
+            for (int c = 0; c < n; c++)
+                maxGold = Math.max(maxGold, findMaxGold(grid, m, n, r, c));
+        return maxGold;
     }
     
-    public void findMaxPath(int[][] grid,int row, int col, boolean[][] visited, int pathSum){
-        
-            maxPathSum=Math.max(pathSum,maxPathSum);
-        
-            visited[row][col]=true;
-            if(row+1<grid.length && visited[row+1][col]==false && grid[row+1][col]!=0)
-                findMaxPath(grid,row+1,col,visited, pathSum + grid[row+1][col]);
-            if(row-1>=0 && visited[row-1][col]==false && grid[row-1][col]!=0)
-                findMaxPath(grid,row-1,col,visited, pathSum + grid[row-1][col]);
-            if(col+1<grid[0].length && visited[row][col+1]==false && grid[row][col+1]!=0)
-                findMaxPath(grid,row,col+1,visited, pathSum + grid[row][col+1]);
-            if(col-1>=0 && visited[row][col-1]==false && grid[row][col-1]!=0)
-                findMaxPath(grid,row,col-1,visited, pathSum + grid[row][col-1]);
-            visited[row][col]=false; 
-          
+    int[] DIR = new int[]{0, 1, 0, -1, 0};
+    int findMaxGold(int[][] grid, int m, int n, int r, int c) {
+        if (r < 0 || r == m || c < 0 || c == n || grid[r][c] == 0) return 0;
+        int origin = grid[r][c];
+        grid[r][c] = 0; // mark as visited
+        int maxGold = 0;
+        for (int i = 0; i < 4; i++)
+            maxGold = Math.max(maxGold, findMaxGold(grid, m, n, DIR[i] + r, DIR[i+1] + c));
+        grid[r][c] = origin; // backtrack
+        return maxGold + origin;
     }
 }
