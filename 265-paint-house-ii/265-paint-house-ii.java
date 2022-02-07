14a79b82b0e966d4cfadc48414e62412a8d001d5
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-02-06 21:49:20 -0500

    Time: 5 ms (42.49%), Space: 45.2 MB (13.55%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.java
+++ 265-paint-house-ii/265-paint-house-ii.java
@@ -1,21 +1,46 @@
 class Solution {
     public int minCostII(int[][] costs) {
         int minCost=Integer.MAX_VALUE;
-        for(int row=1;row<costs.length;row++){
-            
-            for(int col=0;col<costs[0].length;col++){
-                int minSum= Integer.MAX_VALUE;
-                for(int temp=0;temp<costs[0].length;temp++){
-                    if(temp!=col){
-                       minSum = Math.min(costs[row-1][temp],minSum);
-                    }
-                }
-                costs[row][col] =  costs[row][col]+ minSum;
+        
+        int least = Integer.MAX_VALUE;
+        int sLeast = Integer.MAX_VALUE;
+        
+        for(int col=0;col<costs[0].length;col++){
+            if(costs[0][col]<=least){
+                sLeast = least;
+                least = costs[0][col];
+            } else if(costs[0][col]<sLeast){
+                sLeast= costs[0][col];
             }
         }
         
+        for(int row=1;row<costs.length;row++){
+            
+            int tempLeast=Integer.MAX_VALUE;
+            int tempSLeast = Integer.MAX_VALUE;
+            for(int col=0;col<costs[0].length;col++){
+               
+                if(costs[row-1][col]==least){
+                     costs[row][col] =  costs[row][col] + sLeast;
+                } else {
+                     costs[row][col] =  costs[row][col] + least;
+                }
+                if(costs[row][col]<=tempLeast){
+                    tempSLeast = tempLeast;
+                    tempLeast = costs[row][col];
+                } else if(costs[row][col]<tempSLeast){
+                    tempSLeast= costs[row][col];
+                }
+                
+               
+            }
+            least= tempLeast;
+            sLeast=tempSLeast;
+            
+        }
         
-         for(int col=0;col<costs[0].length;col++){ 
+        
+        for(int col=0;col<costs[0].length;col++){ 
             minCost = Math.min(costs[costs.length-1][col],minCost); 
         }
        
