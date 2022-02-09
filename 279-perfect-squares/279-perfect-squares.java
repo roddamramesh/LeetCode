c697f17ce7243284beec9a75424ea4c69805200d
Ramesh Roddam <roddam.ramesh@gmail.com>
2022-02-08 23:39:23 -0500

    Time: 69 ms (46.96%), Space: 43.9 MB (28.84%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -4,10 +4,13 @@ class Solution {
         int[] sumOfMinSquares = new int[n+1];
         sumOfMinSquares[0] = 0;
         sumOfMinSquares[1] = 1;
-        int minSum=Integer.MAX_VALUE;
+      
         int squareStartVal=1;
         for(int pos=2;pos<=n;pos++)
         {
+            int minSum=Integer.MAX_VALUE;
+            squareStartVal=1;
+            
             while(pos-(squareStartVal*squareStartVal)>=0){
                 int square = squareStartVal*squareStartVal;
                 if(pos-square>=0 && sumOfMinSquares[pos-square]<minSum){
@@ -17,8 +20,8 @@ class Solution {
                 }
                 squareStartVal++;
             }
-            squareStartVal=1;
-            minSum=Integer.MAX_VALUE;
+            
+           
         }
         return sumOfMinSquares[n];
     }
